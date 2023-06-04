37416f3957e07b9184dd53d736c947051e434433
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-06-04 23:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const div=document.getElementById('sizeInfo');
+
+function screenResize() {
+	const heading=document.createElement('h1');
+
+	const x=window.screen.width;
+	const y=window.screen.height;
+	
+	heading.innerHTML=`Width: ${x} and Height: ${y}`
+
+	div.appendChild(heading);
+}
+
+window.addEventListener('resize',screenResize);
